ac741314652902c42b44710bd84bb02aa444dc74
melasculla <youtuba200@gmail.com>
2024-06-10 17:10:09 +0300

    fix auth #2

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,4 +1,7 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
+   if (import.meta.server && to.path === '/') {
+      console.warn('test', useRequestEvent()?.context.user)
+   }
    if (import.meta.client) {
       await useAuth().checkAuthCookie()
    }

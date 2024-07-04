7864bba520204d1288101e5873d1b6210d1ba567
melasculla <youtuba200@gmail.com>
2024-07-04 15:36:57 +0300

    test directive #2

--- plugins/load-directive.ts
+++ plugins/load-directive.ts
@@ -2,11 +2,11 @@ export default defineNuxtPlugin(nuxtApp => {
    const imageProps = {
       class: 'animate-spin',
       src: '/loader.svg',
-      loading: 'lazy',
-      test: 'test'
+      loading: 'lazy'
    }
    nuxtApp.vueApp.directive<HTMLImageElement>('loader', {
       beforeMount: (image, binding) => {
+         console.log(binding.value)
          const src = binding.value
          if (!src) return
 
@@ -15,16 +15,19 @@ export default defineNuxtPlugin(nuxtApp => {
             image.setAttribute('data-src', binding.value)
             image.setAttribute('loading', 'lazy')
             image.classList.add('animate-spin')
+            console.log('set default props')
          }
          const loadImage = () => {
             const img = new Image()
+            console.log('loading image..')
             img.onload = () => {
                image.after(img)
                image.classList.remove('animate-spin')
                image.removeAttribute('data-src')
                image.src = src
                img.remove()
-            }
+               console.log('image loaded')
+         }
             img.src = src
             window.removeEventListener('load', loadImage)
          }
